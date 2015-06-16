856846b715dc0ec9bfe4d32e837692d948cf92dd
xuyao <xy9123@126.com>
2015-06-16 11:05:56 +0800

    add assets admin.css

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,8 @@ module Kuaigou
     # Enable the asset pipeline
     config.assets.enabled = true
 
+     config.assets.precompile += %w[admin/admin.css]
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
